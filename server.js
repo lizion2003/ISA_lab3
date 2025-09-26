d93bc68a77dad496c769a5fbe27115842a1cb418
lizion2003 <156731345+lizion2003@users.noreply.github.com>
2025-09-26 16:45:46 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,40 +2,28 @@ let http = require('http');
 let url = require('url');
 let { getDate } = require('./modules/utils');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8000;
 
 http.createServer(function (req, res) {
 
     let parsedUrl = url.parse(req.url, true);
     let pathname = parsedUrl.pathname; // Get the path
+
     let query = parsedUrl.query; // Get query parameters
+    console.log(query);
 
-    if (req.method === 'GET' && pathname === '/COMP4537/labs/3/getDate') {
-
+    if (pathname === '/COMP4537/labs/3/getDate/') {
         let name = query.name;
 
         let currentDate = getDate();
 
-        let message = `Hello, ${name}, What a beautiful day. Server current date and time is ${currentDate}`;
+        let message = `Hello ${name}, What a beautiful day. Server current date and time is ${currentDate}`;
     
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(`
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Lab3 Part B</title>
-                <meta charset="UTF-8">
-                </head>
-                <body>
                 <p style="color: blue; font-size: 16px;">${message}</p>
-                </body>
-                </html>
             `)
     }
-
-
-
-
 
 }).listen(PORT, () => {
 
