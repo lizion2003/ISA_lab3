9823d66c41fdd6735ea6f4c2c30ae8bf3b062d34
lizion2003 <156731345+lizion2003@users.noreply.github.com>
2025-09-26 16:17:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const mo = require('./modules/math.js');
+console.log(`Hello Zion Li. 3 + 4 = ${mo.add(3,4)}`);
+
+console.log(`Hello Zion Li. 3 - 4 = ${mo.subtract(3,4)}`);
---
+++ server.js
@@ -0,0 +1,43 @@
+let http = require('http');
+let url = require('url');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+
+    let parsedUrl = url.parse(req.url, true);
+    let pathname = parsedUrl.pathname; // Get the path
+    let query = parsedUrl.query; // Get query parameters
+
+    if (req.method === 'GET' && pathname === '/COMP4537/labs/3/getDate') {
+
+        let name = query.name;
+
+        let currentDate = getDate();
+
+        let message = `Hello, ${name}, What a beautiful day. Server current date and time is ${currentDate}`;
+    
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Lab3 Part B</title>
+                <meta charset="UTF-8">
+                </head>
+                <body>
+                <p style="color: blue; font-size: 16px;">${message}</p>
+                </body>
+                </html>
+            `)
+    }
+
+
+
+
+
+}).listen(PORT, () => {
+
+    console.log(`Server is running on port ${PORT}`);
+    
+});
